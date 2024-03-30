451c1dfbcd388f4971292675ded158738b029404
PaulHardman <paul.hardman@victorianplumbing.co.uk>
2024-03-30 11:49:25 +0000

    wip

--- app/main.py
+++ app/main.py
@@ -1,7 +1,15 @@
 from flask import Flask
- 
+from completionCall import call_playlist_prompt
+
 app = Flask(__name__)
  
 @app.route("/")
 def home_view():
         return "<h1>Welcome to Geeks for Geeks</h1>"
+
+
+@app.route('/post-json', methods=['POST'])
+def handle_json_data():
+    return call_playlist_prompt()
+
+
